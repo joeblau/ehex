5f510854640306068e3784eb736fd08974915b87
Joe Blau <im@joeblau.com>
2025-05-07 20:12:57 -0700

    feat: add login page

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,13 @@
-import { http, createConfig } from 'wagmi'
+import { porto } from 'porto/wagmi'
+import { http, createConfig, createStorage } from 'wagmi'
 import { mainnet, sepolia } from 'wagmi/chains'
 
 export const config = createConfig({
   chains: [mainnet, sepolia],
+  connectors: [porto()],
+  storage: createStorage({
+    storage: typeof window !== 'undefined' ? window.localStorage : undefined,
+  }),
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
